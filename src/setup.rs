55d174fb2f201a44b82a7d957aa8ba680525f01b
cthvlab <60858283+cthvlab@users.noreply.github.com>
2025-04-17 16:41:10 +0300

    Update setup.rs

--- src/setup.rs
+++ src/setup.rs
@@ -8,139 +8,107 @@ use colored::Colorize;
 // Дефолтный config.toml как строка
 const DEFAULT_CONFIG_TOML: &str = r#"
 trusted_host = "localhost"
+# Режим интерфейса: console, gui, 3d
+graphics = "gui"
 
-http_port = 8888 		# Порт для старых шлюпок (HTTP)
-https_port = 8443 		# Порт для бронированных крейсеров (HTTPS)
-quic_port = 8444 		# Порт для гиперскоростных звездолетов (QUIC)
+http_port = 8888
+https_port = 8443
+quic_port = 8444
 
-# Где лежит сундук с шифрами
 cert_path = "cert.pem"
-# Где лежит ключ от сундука	
 key_path = "key.pem"
-# Где лежат свитки рейда	
 log_path = "yuaiserver.log"
-# Тайный пиратский код для проверки пропусков	
 jwt_secret = "password"
 
-# Лимиты скорости — сколько добычи можно утащить за раз!
-guest_rate_limit = 10          # Лимит для гостей, что шатаются без дела
-whitelist_rate_limit = 100     # Лимит для друзей капитана, что в белом списке
-blacklist_rate_limit = 5       # Лимит для шпионов, чтоб не лезли слишком часто
-rate_limit_window = 60         # Сколько секунд ждать, пока трюм снова откроется
-# Ограничение на добычу в трюме, чтоб шпионы не затопили корабль!
-max_request_body_size = 1048576 # 1 МБ — предел трюма для запросов, больше не лезет!
+guest_rate_limit = 10
+whitelist_rate_limit = 100
+blacklist_rate_limit = 5
+rate_limit_window = 60
+max_request_body_size = 1048576
 
-# Маяки для телепортации
 ice_servers = ["stun:stun.l.google.com:19302", "stun:stun.example.com:3478"]
+
+# Глобальные настройки статики
+[static_settings]
+static_extensions = ["html", "css", "js", "png", "jpg", "jpeg", "gif", "ico", "svg", "woff", "woff2"]
+default_csp = "default-src 'self'"
 
 # Космический атлас — куда лететь и что отдавать!
 [[locations]]
 path = "/"
 response = "Добро пожаловать в космопорт, пришелец!"
+route_type = "monolith"
+csp = "default-src 'none'"
 headers = [
     ["Content-Type", "text/plain; charset=utf-8"],
     ["X-Welcome", "Yo-ho-ho"]
 ]
+
 [[locations]]
 path = "/api"
 response = "{\"message\": \"Это API endpoint планеты Солярис!\"}"
+route_type = "microservice"
 headers = [
     ["Content-Type", "application/json"]
 ]
+
 [[locations]]
 path = "/static"
 response = "Через гиперпространство в Торговый Пост, йо-хо-хо!"
+route_type = "static"
 "#;
 
 // Дефолтный самоподписанный сертификат
 const DEFAULT_CERT_PEM: &str = r#"-----BEGIN CERTIFICATE-----
-MIIF7zCCA9egAwIBAgIUR6/I2ZFU4r5vkZKHeUEswlQULZQwDQYJKoZIhvcNAQEL
-BQAwgYYxCzAJBgNVBAYTAlJVMRQwEgYDVQQIDAtUYW1ib3YgYXJlYTEPMA0GA1UE
-BwwGVGFtYm92MQ0wCwYDVQQKDARZVUFJMRMwEQYDVQQLDAp5dWFpc2VydmVyMRIw
-EAYDVQQDDAlsb2NhbGhvc3QxGDAWBgkqhkiG9w0BCQEWCWlAeXVhaS5ydTAeFw0y
-NTAzMDkxMjI2NThaFw0yNjAzMDkxMjI2NThaMIGGMQswCQYDVQQGEwJSVTEUMBIG
-A1UECAwLVGFtYm92IGFyZWExDzANBgNVBAcMBlRhbWJvdjENMAsGA1UECgwEWVVB
-STETMBEGA1UECwwKeXVhaXNlcnZlcjESMBAGA1UEAwwJbG9jYWxob3N0MRgwFgYJ
-KoZIhvcNAQkBFglpQHl1YWkucnUwggIiMA0GCSqGSIb3DQEBAQUAA4ICDwAwggIK
-AoICAQCZQ2UI5W2z8L1a3PxnfKBOa/wWmPxOaZ/OEiUheal6Wqsh17vHk5Uemp82
-+KxzbobDngZjNevWT4fPyoH/I2X/b92H0Gp9L4KitunS4mbCVLoFqCogtGpRhH2w
-JLfcS7xW0etagVQMS55xyEAdDb334n/6s8duN0CueIQWeDerzkbdQOfhNFDnTua4
-T1ttb0QapYkpbNj6WoygoDr7fXuIKHfnK+IiNpE6t74zRxvIDSVqQTsms1qhvD2x
-aXtVIuqGk99H+fkmgDf6oUA5SlHO5tevfLM09YidRKVQxa3TZhck0LWVcIqlI/3v
-YzAvlXWMZapKxYewLbePtisdvJxDuSmLg3OWTLFF1YOiJsEeBrBqnvBZTnitnmZ8
-jZbPLnNJBuu+TgcxG8qk5xgqTBLMu1jP7aDpv7A8jcOy7IURyxfGQvlm8oa9bJxi
-DIfXlLPpVMp0cQ3EUvhdCw58DOn1HwMTyTJDZ3s76bcyuREmsBM38nIpGuZBwvLv
-j4TTu7WnSlrzaUKpDM53e4mJuTYAP3fjGeOfBhY0ev+YySuHEUTlvZcVI/jfkFEC
-jjkqttdRsVPdwciGvp3w0PAJ2GPVjvIpUhUaPzFDZPUG0oRaPq8lZU/4sfVvuVbf
-lqvXs+scW5w6FnJPm9Nczv8dcKemxDRgtGmmFzIdv4+HlUIf0QIDAQABo1MwUTAd
-BgNVHQ4EFgQUyoPO7VzjSSjS1xQP4GrcMptGsZgwHwYDVR0jBBgwFoAUyoPO7Vzj
-SSjS1xQP4GrcMptGsZgwDwYDVR0TAQH/BAUwAwEB/zANBgkqhkiG9w0BAQsFAAOC
-AgEAD2JLP1xcZOhOJtorRt72hiIT6w6lmZc+0oIUH6ObdTwEMXfBgxKTsZQY0Ho6
-8oz/Gx6dspTaH34FuYZS1GgljBNxOMRq3GWrIjDCFyZ9D/QaF2KrhBtTfear2cRl
-zUIHTn8F2Ee/nr3eL6MwgT+xINq/8qkcYoCkshDn/iXAfM6XEne32U0V8esVh0yR
-otiXWiuTrpuPb1YtWxUstjwIqx7T6PROAb1L3mPpCY8zwOJxVecJtyKIeEWXMs8n
-3rczFOIhmjLa3tdMKgHSVWcxwWolUuKdYvO4Tbc4cWtH0Ndio5XqH2LejsVfPq4Q
-tXaH7Sfhvil+zIRhA9kKmWuKd3OftseILg6zjb8VS+FZMYpegn7xAqbKPWX3UJvu
-0jtNG7zGRr9Am9KOP31uZFAZYBDS5Yc8MUpetFvNvVrOUtkZWRjPOLtWTePmHzWn
-K6aRs72g0BHqrXjf4qc0VYDBSWOBXbi5I6tNimQeqr/CyYUmyydVp+b92ly6mXsH
-i7mSF70euULl18XCoSbYH/d7OCqE6U6YwAB2Wch6+Gah+F2R/OD+M+VIO93usO2j
-lPCqC0oLPoKEDaWr6xAONRYf4c9lDpze5j3+fDBqGqB0c71FlOVqWClhq5g0CLm1
-PuM8m1mdAjwpurT9FiRHBy15QBKdJdSLJGboc07TKrgO168=
+MIIDODCCAiCgAwIBAgIUFytf5mXN0XjloQyk42U9TtDsGtUwDQYJKoZIhvcNAQEL
+BQAwFDESMBAGA1UEAwwJbG9jYWxob3N0MB4XDTI1MDQxMTA1NDI0OFoXDTI2MDQx
+MTA1NDI0OFowFDESMBAGA1UEAwwJbG9jYWxob3N0MIIBIjANBgkqhkiG9w0BAQEF
+AAOCAQ8AMIIBCgKCAQEA2V/18SPjiU6XeibQ9u8H9eWdwjRGvNwh459UMfZbEYT7
+FQeLsGvnw0in8y8ZqBCA+j/1R1ImI+IFA7x/jrLPKJMmtv+89m2XLMuJqg/HDVF0
+a9rcoui/IDSfPsxhdOqyhBuT4SeEd8+Oxe2wFLOOzA12iAqHYkGbm98pViOv7mbP
+Zp+INhP5YrIENXsTUKgBYYEs0tAiJ4FU5Zcq/Hx1DNHdYReZ6mIQMwT3QxUTTdv4
+U470e3a7vfqbA106qgK0Gx7ImFbEgotgFlco4nwJ+zaBUpUlhdaTnD9aUPFhBRob
+4sN1R7ysln/2i6UXUQL8tr6W33VM5gix3lnlmyyLpQIDAQABo4GBMH8wHQYDVR0O
+BBYEFHHezmlPDItLpfTBZ4YfAQp5m8+CMB8GA1UdIwQYMBaAFHHezmlPDItLpfTB
+Z4YfAQp5m8+CMA8GA1UdEwEB/wQFMAMBAf8wLAYDVR0RBCUwI4IJbG9jYWxob3N0
+hwR/AAABhxAAAAAAAAAAAAAAAAAAAAABMA0GCSqGSIb3DQEBCwUAA4IBAQAk2jiK
+Dv2dnSJ5zgE/nqK/zg94HbHZFpqie77oiVoktWaHkzBbQTJ1kuyorTH0Rq8ZHI1V
+Eny4tYJUsRNKnJgeGShPRX68HnFB3DBwTbW8oKK6lrZo+siEUWENdHRUb4bNg79K
+gjIu3na/V3YW1+soAV0id8Ow9Xkxz3aU1t5PURV8c5Bdhw2jPeax+l67TiF9+aF6
+tzJFloEnQWTB4ZHnFYomkHmB12pqUMR6k8lAWhqcm2v10m3u6IFC1f4rnPouJ/PI
+a8mgeQleNPoF7QlFj50Q7j+vuOBeZuUgThHgX9erCG1TPmWGNs/89FA97zAe3qK2
+Bwx/o7Xh50IcJ9LX
 -----END CERTIFICATE-----
 "#;
 
 // Дефолтный приватный ключ
 const DEFAULT_KEY_PEM: &str = r#"-----BEGIN PRIVATE KEY-----
-MIIJQwIBADANBgkqhkiG9w0BAQEFAASCCS0wggkpAgEAAoICAQCZQ2UI5W2z8L1a
-3PxnfKBOa/wWmPxOaZ/OEiUheal6Wqsh17vHk5Uemp82+KxzbobDngZjNevWT4fP
-yoH/I2X/b92H0Gp9L4KitunS4mbCVLoFqCogtGpRhH2wJLfcS7xW0etagVQMS55x
-yEAdDb334n/6s8duN0CueIQWeDerzkbdQOfhNFDnTua4T1ttb0QapYkpbNj6Woyg
-oDr7fXuIKHfnK+IiNpE6t74zRxvIDSVqQTsms1qhvD2xaXtVIuqGk99H+fkmgDf6
-oUA5SlHO5tevfLM09YidRKVQxa3TZhck0LWVcIqlI/3vYzAvlXWMZapKxYewLbeP
-tisdvJxDuSmLg3OWTLFF1YOiJsEeBrBqnvBZTnitnmZ8jZbPLnNJBuu+TgcxG8qk
-5xgqTBLMu1jP7aDpv7A8jcOy7IURyxfGQvlm8oa9bJxiDIfXlLPpVMp0cQ3EUvhd
-Cw58DOn1HwMTyTJDZ3s76bcyuREmsBM38nIpGuZBwvLvj4TTu7WnSlrzaUKpDM53
-e4mJuTYAP3fjGeOfBhY0ev+YySuHEUTlvZcVI/jfkFECjjkqttdRsVPdwciGvp3w
-0PAJ2GPVjvIpUhUaPzFDZPUG0oRaPq8lZU/4sfVvuVbflqvXs+scW5w6FnJPm9Nc
-zv8dcKemxDRgtGmmFzIdv4+HlUIf0QIDAQABAoICAACJC/q3OcYfC+ZGmdTEX8H5
-CMEic9UpSMndOzHN/Tx5wcRv6WPAfUzv+344vvogk3TDofWUQWM3ABGG0qPrwXuI
-2OmpyHQbUks+X3w2rBG3M+F5AtFTmAB6Dun/2Crl59NCJJ6PukBYaPCwTfWxWoMZ
-uy/gE2JW6+aqx0QAflSfDJUmw3iVqHJaY/NrsYp6ZkQ/5PCsWj4A9XdKf/zourb7
-yd7DQuUIMu3VKPirDFWLTYtnYsJsmMndALZ/LhNazci+Y6avy0HqFj1NoA4N11HP
-8XEXOtVZ8IojbH68ULJe66tGVbAJ29jI/VDUqZ0s744X59D0Ni20cdedFQhOTu8V
-50UzVqyruc1VmjuYZPKesF1rxlnK4FECQJw00Dmw5UILDryy68UUBbQHsbGcfNYh
-4uKWMpjxTJ3dfcgfxOQKk1ghssA3EcttJ8XAO0SaGcrwDDqitntXkWYnBae7wXKE
-UFaLytvxsCzi/hLY7/K0ax8/UDujrSgUZe3SXlIomcn+Swhl/1xRg7X4Om5TwhG/
-Y9Fln4Tw+ROzOFRsO2PP74DOgLEt5v9HxeRFM+hSf+QK4U58vEStAxyT51xdyr8C
-oluzGZJCDCrqziM1S953qBzi0uoJ5F6Mwvhd4D0li9yRccpUO1wv/kcebq1IOA04
-0HkqEEwILN0N5rxbHB3dAoIBAQDV9I0i44AGWSjDc03190a3Oil+5Svh7bYQpYPs
-gXIIib4zFdjHI7BB0su16lm+WyRmJF4hnNo60oCgbYbNoAkOAPOYpkTIuVD6MV3o
-DuO0S9uj03MP4bDuEzWRHsFaj5ELYWMVCX2QvKNufOa28I+FoNhSz3vehGtiU42i
-3Wk93VWoFHHY0ZyG1tc8Yy4XK5yODE14V28qaMXs0Py3xutpS1fBqAb81lom93jr
-rvFaqumDzm/FkMal7mwXq8+OmquldS+gcF9RPlt3mmlAYQfC1cGZfrtl4yW2xjHP
-X+36hjdBGtQqceQbwJhxuwKAFau2cJlqbClcGJ0U8OmINlQNAoIBAQC3YZwfp3fF
-xM1PrqDWzzr3gVCINWlfX8LSrSraMZin+RSt301ChTF/WYyEJkwcRG42DxrBdy7a
-oVkFrpOB8RTkK+6rM6NXvEfg0LNu6Yd0PAYKnbVOZwFaMB04W4wDghDX3vs0f4zY
-YbEtFWKkUyDeCUDZV/00CWQP6HqpHb2FRAmY7UsDPqa11ehyJOXn3Ucg5f+QTrN0
-ZlGc91+EUpKIN1madK0ggdbW2FMC+1T0rYZZTdlLzE4uW4tqlmap1a+vHD2tnN/j
-IOOrr2ffZsc5r5d+4JVk9q0FPs4x+ihnSD//LAHmwfpXksgLHLl7zKo4Iqwg736g
-MNLYhMuD5bXVAoIBAFDOfD+hDMlZAzxOYDN1oQ+yt9llVJ2P2iOPX7QDb74px9wj
-dCxCHl+97ZJXYG20npq1QZgQVZvBxTLSKweeWc0gLuCIa5Ij66GS45Sxiu6dd+b4
-BY0KAHjqaGDW7M/3SSCzN2jKnNH9bPxd0AVn9czSYuFj27S+7o/EXsoUNZtX7PH+
-RsFM6YFDwybbjpzRq986Zkf9Pc8Woyo+BDSkvGSYQBy0CW+UxZfR/6mD+UP1KRzO
-6wgDFgO1eL239jw1zn4/NC20Q0u78W/KsdXOz8+WS/hdFQn2MFhHoqhf4tGrv1Kn
-f1pIiY6xvuG1b8xQYvoUrNy03SobYbh3BLydSEECggEBAJtnGpkwoC1208583Ext
-nTj12zsymDN8wWsr2K7DbOycBkw6egyvNv0G9C9hDQq/au5g6nmc+JXKOdi40sRB
-e0TJfh6TSg8lvXvlIMoTGPkFjNEeSPFCFUFKmNiTrBmk9YUar6MTnFYao7zi7I74
-61d+W/hTun2In8Vja985FsyCleeE4BbF1x0tQyjH4BUXzhkxD9xI9Ybk1rDAaa+H
-EiphTYu46KYtodS7yc0zFBGto7a2ZDj50B808PbIsgOm1JT+x74CvTCLRKTPpqUJ
-Qje+eBHLXI8NBB731GFB709nSOSDdLELHY1tFB1moEErdezQZTIcAyDZfzSBhL4n
-wmkCggEBALP67dSFnlhRm6CBo2jqnMQ5cEYLGMhjX4mMc9+8Orzd9ZjoE5OoWpXf
-SsVled6WweTLRyEmb3fhSxNd6i5Eenr/UNFRtfT3EIImn9FNKAcuEFJP4KpBPcO+
-7FaHl9xgX/62aJv+O1EDbJuGGVB1/VNU+r8umuIvIez4jpaAwoNYWwHjAnqWyC6T
-hqcqZ921O1jrSHJIgpGocikD+ooBUWcaOz0CUsZEW6NcU1/brsYje9RB3kMPuINA
-DXtwAX25SBUcEdJatKQcsh9F6eGZOaSZu4zkL0vk5Dw/HqS44G+CVwo2ubfXR1ce
-KKdrsDlYVnmsAs6H0fmxviToaulJUW0=
+MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDZX/XxI+OJTpd6
+JtD27wf15Z3CNEa83CHjn1Qx9lsRhPsVB4uwa+fDSKfzLxmoEID6P/VHUiYj4gUD
+vH+Oss8okya2/7z2bZcsy4mqD8cNUXRr2tyi6L8gNJ8+zGF06rKEG5PhJ4R3z47F
+7bAUs47MDXaICodiQZub3ylWI6/uZs9mn4g2E/lisgQ1exNQqAFhgSzS0CIngVTl
+lyr8fHUM0d1hF5nqYhAzBPdDFRNN2/hTjvR7dru9+psDXTqqArQbHsiYVsSCi2AW
+VyjifAn7NoFSlSWF1pOcP1pQ8WEFGhviw3VHvKyWf/aLpRdRAvy2vpbfdUzmCLHe
+WeWbLIulAgMBAAECggEAFI4z06RxKoAkEt5YzwELilU29du7qgr8VBSyVtx/qshz
+IH9LgQNd3I73qCdsEFh/LyryfCwTLzwlp+ogpdMtg8i71dJD1t8bd0DyuQIvmzLO
+BOT4DZpfeHbWwuQmKHLehApCPFMK/nQXgYqt0UdULuJBszD2XHRCeBrs7cMHf/j2
+6D7xebUQwm/MjGth1Mcm6xMRpoA3gpv1C9H9hBv1GiZmUdk5No6JkPQTrXtT5Fvu
+Bo406Knj5vPhgE+j6b32jtHdCHICQznPdmQVjKYMxlfAG5HNjVWcF20rfyqO7wfy
+8uhS4PORPFwjhkU32FHZ8i+ALunkA3TGxtstHTkGgQKBgQD1YuTnyZZnzqUHkKYH
+AZriKh6VRHds91HhaQya5fO7C2e8NqnVH4yJwq9Ig7xncwczjSGYRuV9PHfgcUOa
+ivZ89D13qeWGS6UdcF5Tc+99h4bOO4MncxkdQIDMS47RwCJ8WrxJ8TdjvP4hlZex
+XvY7iB1/1XwtiUhLBNFD2UHLlwKBgQDixubyrsD94ZOjWrNptqqZ1kGuSxMsCe2z
+77Fkb15EU/8KIhNs5dUUVuBJVy5QqNQCW/Pvv9pO/ouh8XJ0xfcNpW5D8qr2ZcRx
+IBjyQxwFjQ4qlMnLcAVVp5bHO40obeGCqbXbM+LTbXGIomoBlgAadg9eC1HJVK15
+GHYuoQ66IwKBgFnTB6PpYQtC05o/UudBqSac8HEHjQfwSKLQx3J0NpIDjMeS4vxG
+/jT3dR6ASpk7vCkcwm1xllQPrHoWO/74W15GMbH6GTDdw+VQ2taFm+dBkxEvK8Vn
+FoxkrkEab39Ma9NFi6Mtj28NIaG9lrew4UXNf67pwPkSRcpgSxIhFzqlAoGBALKl
+1lHf8REPn2rUjPn+eK7v5pYTdmr+908EyE5CnZReR1AIQB1NgWqgddfQ4h+QoFOr
+dKOuE6CfTgipWG09dr49UHwesyegS/yCOKTA0VZeJIbO55loCgvMFi+lbjInPbvd
+TfH9EfdVBFeK+s99B1/RGZIQgMGF/5Kh/pgFMMDZAoGAG4Z9eZR1ga2Px98mgLMv
+L82hlA+d+2yKkUsq8clDDhoyZ8WeZdi/A26LN2s4UvHmzoCrLVhae4RG9dcnTa7n
+JfPiUGe2kn95PHTOaY4EGgiEQCTFNKCnRxd/jTQAinft8bLv08VggzyksN16NVb6
+mp0YDseTSs+fAtb0X/SQiu4=
 -----END PRIVATE KEY-----
 "#;
 
